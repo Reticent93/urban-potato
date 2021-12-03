bcbbb2b866b5d4cbde55e7f46937d7c11ad8bb25
gren8 <greg.renfro@gmail.com>
2021-12-03 12:15:23 -0700

    added contact page

--- main.go
+++ main.go
@@ -1,1 +1,23 @@
-package urban_potato
+package main
+
+import (
+	"fmt"
+	"net/http"
+)
+
+func handlerFunc(w http.ResponseWriter, r *http.Request)  {
+	w.Header().Set("Content-type", "text/html")
+	if r.URL.Path == "/" {
+		fmt.Fprint(w, "<h1>Welcome to my awesome site!</h1>")
+	} else if r.URL.Path == "/contact" {
+	fmt.Fprint(w, "To get in touch, please send email to <a href=\"mailto.support@urban-potato.com\">support@urban-potato.com</a>.")
+	}
+}
+
+
+func main() {
+	
+	http.HandleFunc("/", handlerFunc)
+	http.ListenAndServe(":3000", nil)
+	
+}
